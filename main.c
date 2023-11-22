52c376f5fc4df5a3738c84bfff10a7a405d0b7e2
seehorne <95896637+seehorne@users.noreply.github.com>
2023-11-22 00:56:50 -0600

    main.c
    pad lines with spaces so we can have a uniform length

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 #include <fcntl.h> 
 #include <sys/stat.h>
 
-#define MAX_LINE_LENGTH 100
+#define MAX_LINE_LENGTH 101 //100 chars plus a null terminator
 #define MAX_LINE_COUNT 40
 
 const char* username;
@@ -40,27 +40,29 @@ int main(int argc, char **argv){
     }
     //printf("did not crash trying to r+x open the file\n");
     //when we launch threads, we probably want the void* to include the global file_rep as well
-    char put_this=fgetc(file);//read the first char of the file, for a starting off place
+    char put_this;
     for (int file_lines = 0; file_lines < MAX_LINE_COUNT; file_lines++){
-        printf("\n");
-        if (put_this == EOF){//if at the start, the file is empty, you can just leave
-            break; //leave the reading loop
-        }
-        for (int chars = 0; chars < MAX_LINE_LENGTH; chars++){//if we didn't just break, start reading characters into lines
-            if (put_this == EOF){//if it's the newline or EOF, stop reading into this line
-                break; //leave the reading loop for this line
+        put_this=fgetc(file);//read the first char of the file, for a starting off place
+        for (int chars = 0; chars < (MAX_LINE_LENGTH-1); chars++){//if we didn't just break, start reading characters into lines
+            if (put_this != EOF && put_this != '\n'){//if it's the newline or EOF, stop reading into this line
+                our_file->contents[file_lines].line_contents[chars]= put_this;
+                put_this = fgetc(file);//read for next time
             }
-            else if (put_this == '\n'){//if it was a newline, add the null terminator to string, then break reading loop
-                our_file->contents[file_lines].line_contents[chars]= '\0';
+            else{
+                if (chars < (MAX_LINE_LENGTH-1)){//not the very last char in array
+                    our_file->contents[file_lines].line_contents[chars]= ' '; //fill up to end of line with spaces, for navigational ease (pointer math of line lengths)
+                }
+                else{//very last char in line should be the null terminator
+                    our_file->contents[file_lines].line_contents[chars]= '\0';
+                }
             }
-            printf("%c", put_this);
-            our_file->contents[file_lines].line_contents[chars]= put_this;
-            put_this = fgetc(file);//read for next time
         }
+        printf("%s\n", our_file->contents[file_lines].line_contents);
         //if the 100 characters was not enough, oops, sorry your line gets cut off, should have read the rules
         while (put_this != '\n' && put_this != EOF){//enters this loop if 100 characters was not enough
             put_this = fgetc(file); //don't store it, just walk through it
         }
+        our_file->contents[file_lines].line_contents[MAX_LINE_LENGTH-1]= '\0';
     }
     fseek(file, 0, SEEK_SET); //puts the pointer back at the start of the file
     fclose(file);
