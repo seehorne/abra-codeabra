2532f684f0d8cec134726e518840f4f16fdebeda
Amani Alqaisi <96408247+Amani-Alqaisi@users.noreply.github.com>
2023-12-13 17:00:53 -0600

    Removed unnecessary commented out lines

--- message.h
+++ message.h
@@ -2,17 +2,6 @@
 
 #define MAX_MESSAGE_LENGTH 2048
 
-// #define MAX_LINE_LENGTH 101 //100 chars plus a newline
-// #define MAX_LINE_COUNT 40
-
-// //structure for a file line, contents char[], int to represent who owns it
-// typedef struct file_line file_line_t;
-
-// //data structure representing the contents of a file
-// typedef struct file_rep file_rep_t;
-
-//structure for a file line, contents char[], int to represent who owns it
-
 // Send a across a socket with a header that includes the message length. Returns non-zero value if
 // an error occurs.
 int send_message(int fd, char* message);
@@ -21,6 +10,4 @@ int send_message(int fd, char* message);
 //freed later).
 // Returns NULL when an error occurs.
 char* receive_message(int fd);
-
-//int send_file(int fd, void* message);
 
